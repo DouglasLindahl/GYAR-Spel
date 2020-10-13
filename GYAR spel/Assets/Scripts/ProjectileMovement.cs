3512f508567571a2256d7fc5d5adc525e3844809
DouglasLindahl <71632655+DouglasLindahl@users.noreply.github.com>
2020-10-13 11:13:24 +0200

    inget

--- GYAR spel/Assets/Scripts/ProjectileMovement.cs
+++ GYAR spel/Assets/Scripts/ProjectileMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ProjectileMovement : MonoBehaviour
 {
+    public float lifetime;
+    public int speed = 50;
     Rigidbody rb;
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = transform.forward * speed * Time.deltaTime;
+        Destroy(gameObject, lifetime);
     }
 }
---
+++ GYAR spel/Assets/Scripts/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Shooting : NetworkBehaviour
+{
+    public GameObject shotPoint;
+    public GameObject projectile;
+    public float shotCooldown;
+    private float shotCooldown2 = 0;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+        Shoot();
+    }
+    public void Shoot()
+    {
+        if(shotCooldown2 <= 0)
+        {
+            if(Input.GetMouseButton(0))
+            {
+                Instantiate(projectile, shotPoint.transform.position, shotPoint.transform.rotation);
+                shotCooldown2 = shotCooldown;
+            }
+        }
+        else
+        {
+            shotCooldown2 -= Time.deltaTime;
+        }
+    }
+}
